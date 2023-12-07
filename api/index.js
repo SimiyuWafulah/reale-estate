54e7eb9d7a0aa1840a9bba794709ade8f16db550
SimiyuWafulah <simiyukelvinse@gmail.com>
2023-12-07 20:49:18 +0300

    connecting to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose'
 import userRouter from './routes/user.route.js'
+import dotenv from 'dotenv'
+dotenv.config();
+
 
 const app = express();
+
+mongoose.connect(process.env.MONGO).then( () => {
+    console.log('Connected to Database');
+}).catch((err) => {
+    console.log(err)
+});
 
 app.listen(3000, () => {
     console.log('Server is running!')
