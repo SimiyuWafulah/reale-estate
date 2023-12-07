170b2ab78a299bd5cd43a3f6d11826279419e286
SimiyuWafulah <simiyukelvinse@gmail.com>
2023-12-07 20:30:21 +0300

    creating test api routes

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import userRouter from './routes/user.route.js'
 
 const app = express();
 
 app.listen(3000, () => {
     console.log('Server is running!')
 });
+
+app.use('/api/user', userRouter);
