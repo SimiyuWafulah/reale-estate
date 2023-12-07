746da8f2ade5ccc29d866dd3f787de128338aa9d
SimiyuWafulah <simiyukelvinse@gmail.com>
2023-12-07 22:32:39 +0300

    completed sign up page functionality

--- api/index.js
+++ api/index.js
@@ -4,12 +4,16 @@ import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import dotenv from 'dotenv'
 dotenv.config();
+import cors from 'cors';
 
 
 const app = express();
 
 app.use(express.json());
-
+app.use((cors({
+    methods:['POST','GET', 'DELETE'],
+    origin:"*"
+})))
 mongoose.connect(process.env.MONGO).then( () => {
     console.log('Connected to Database');
 }).catch((err) => {
