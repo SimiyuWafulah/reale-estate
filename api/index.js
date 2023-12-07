3a10d3142020c9148eba75d5d1bf0ac19dbdf055
SimiyuWafulah <simiyukelvinse@gmail.com>
2023-12-07 21:04:55 +0300

    user model

--- api/index.js
+++ api/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose'
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 import dotenv from 'dotenv'
 dotenv.config();
 
 
 const app = express();
+
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO).then( () => {
     console.log('Connected to Database');
@@ -18,3 +21,4 @@ app.listen(3000, () => {
 });
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
